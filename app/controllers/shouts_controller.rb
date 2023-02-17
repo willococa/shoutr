f9d9d1830bbed30113893617e455f400b45637df
demondzeta <williamjocoro@gmail.com>
2023-02-17 00:34:14 -0500

    aded shouts and listed

---
+++ app/controllers/shouts_controller.rb
@@ -0,0 +1,16 @@
+class ShoutsController < ApplicationController
+	def create
+		shout = current_user.shouts.create(shout_params)
+		redirect_to root_path, redirect_options_for(shout)
+	end
+	def shout_params
+		params.require(:shout).permit(:body)
+	end
+	def redirect_options_for(shout)
+		if shout.persisted?
+			{notice: "Shout succesfully created!"}
+		else
+			{alert: "could not shout!!"}
+		end
+	end
+end
