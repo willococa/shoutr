fbd5cfbe2729eb64e4d5ff20a6628d9756ded954
demondzeta <williamjocoro@gmail.com>
2023-03-06 06:22:31 -0500

    add photoshouts

--- app/controllers/shouts_controller.rb
+++ app/controllers/shouts_controller.rb
@@ -10,16 +10,23 @@ class ShoutsController < ApplicationController
 	def shout_params
 		{content: content_from_params}
 	end
-	def content_from_params	 
-		TextShout.new(content_params)
+	def content_from_params
+		case params[:shout][:content_type]
+			 when "TextShout" then 	TextShout.new(text_shout_content_params)
+			 when "PhotoShout" then 	PhotoShout.new(photo_shout_content_params)		 	
+		 end
 	end
-	def content_params
+	def text_shout_content_params
 		params.require(:shout).require(:content).permit(:body)
+	end
+	def photo_shout_content_params
+		params.require(:shout).require(:content).permit(:image)
 	end
 	def redirect_options_for(shout)
 		if shout.persisted?
 			{notice: "Shout succesfully created!"}
 		else
+			#debugger
 			{alert: "could not shout!!"}
 		end
 	end
---
+++ app/models/photo_shout.rb
@@ -0,0 +1,5 @@
+class PhotoShout < ApplicationRecord
+  has_one_attached :image do |attachable|
+    attachable.variant :thumb, resize_to_limit:[200,200]
+  end
+end
