bb005f17f8e88770aa1bbced23c943babf8b8557
demondzeta <williamjocoro@gmail.com>
2023-03-07 05:40:42 -0500

    added followers functionality and list

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,15 +4,22 @@ class User < ApplicationRecord
   has_many :shouts, dependent: :destroy
   has_many :likes
   has_many :liked_shouts, through: :likes, source: :shout
-  has_many :following_relationships, foreign_key: :follower_id
-  has_many :followed_users, through: :following_relationships
-
+  has_many :followed_user_relationships, 
+    foreign_key: :follower_id,
+    class_name: "FollowingRelationship",
+    dependent: :destroy
+  has_many :followed_users, through: :followed_user_relationships
+  has_many :follower_relationships,
+    foreign_key: :followed_user_id,
+    class_name: "FollowingRelationship",
+    dependent: :destroy
+  has_many :followers, through: :follower_relationships
   def follow(user)
     followed_users <<  user
   end
   def unfollow(user)
     followed_users.delete(user)
-  end
+  end 
   def following?(user)
     followed_user_ids.include?(user.id)
   end
