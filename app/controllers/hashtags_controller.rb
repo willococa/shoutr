6897de144e3e7ee5bb6e83cc9a37a24050bbd0cb
demondzeta <williamjocoro@gmail.com>
2023-03-08 05:25:09 -0500

    added search object

--- app/controllers/hashtags_controller.rb
+++ app/controllers/hashtags_controller.rb
@@ -1,7 +1,11 @@
 class HashtagsController <  ApplicationController
 	def show
 		@hashtag = params[:id]		
-		@results = Shout.joins("LEFT JOIN text_shouts ON content_type = 'TextShout' AND content_id = text_shouts.id").
-			where("text_shouts.body LIKE ?", "%##{@hashtag}%")
+		@search = Search.new(term: hashtag)
+	end
+
+	private
+	def hashtag
+		params[:id]
 	end
 end
